e6a97ea7383515ebc4af10b84959981f577f74f0
Alexandre Tavares <alexandre.j.tavares.jr@gmail.com>
2025-05-27 19:46:36 -0300

    fix: adicionando modelo

--- api/services/prediction_service.py
+++ api/services/prediction_service.py
@@ -1,7 +1,7 @@
 import numpy as np
 from tensorflow.keras.models import load_model
 
-model = load_model("models/lstm_model.h5")
+model = load_model("models/lstm_model.keras")
 
 def predict_price(prices: list[float]) -> float:
     input_array = np.array(prices).reshape(1, -1)
